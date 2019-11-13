29b18dad590a6cec59415fadb8a5fbcc515e3999
César Alberca <cesalberca@gmail.com>
2019-11-13 20:26:18 +0100

    Use generics

--- src/proxies/safeAccess.ts
+++ src/proxies/safeAccess.ts
@@ -1,6 +1,6 @@
-export function createSafe(target: object): object {
+export function createSafe<Target extends object>(target: Target): Target {
   const handler = {
-    get(target: object, name: string | number, receiver: any) {
+    get(target: Target, name: string | number | symbol, receiver: any) {
       if (hasKey(target, name)) {
         const targetElement = Reflect.get(target, name, receiver)
 
@@ -29,4 +29,4 @@ export const notDefined: object = new Proxy(
 export const either = <T>(value: object, fallback: T) => (value === notDefined ? fallback : value)
 
 const isObject = (obj: object) => typeof obj === 'object'
-const hasKey = <T>(obj: T, key: string | number | symbol) => key in obj
+const hasKey = <Object>(object: Object, key: string | number | symbol) => key in object
