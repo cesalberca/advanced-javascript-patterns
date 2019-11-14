12b9015e3efd7a9ddaa506f2db04a2a35a78e2fe
César Alberca <cesalberca@gmail.com>
2019-11-14 23:45:44 +0100

    Update test configuration

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  presets: ['@babel/env'],
-  plugins: ['@babel/plugin-transform-modules-commonjs']
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ]
 }
